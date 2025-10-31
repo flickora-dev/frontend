a33b0a321fbbb9daa9665873767e3fc12a68b49a
Влад Усатенко <245127@edu.p.lodz.pl>
2025-10-31 19:30:20 +0100

    Removed SessionAuthentication, using JWT authentification instead

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
+  withCredentials: false,
 });
 
 // Request interceptor to add auth token
