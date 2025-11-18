ab8405d57b9242a00448d1815478a8cda1ff823f
Влад Усатенко <245127@edu.p.lodz.pl>
2025-11-18 20:05:50 +0100

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(
-      process.env.VITE_API_URL || 'https://flickora-production-7a6e.up.railway.app/api'
-    )
+  server: {
+    port: 5173,
+    strictPort: true,
   }
 })
